b6042c6afeef4cde6b486f1412c5ea8de58c3f32
Ippei Nawate <saku.ni63@gmail.com>
2016-12-29 18:03:33 +0900

    Use databinding

--- app/src/main/java/com/sakuna63/sandbox/MainActivity.java
+++ app/src/main/java/com/sakuna63/sandbox/MainActivity.java
@@ -1,13 +1,16 @@
 package com.sakuna63.sandbox;
 
-import android.support.v7.app.AppCompatActivity;
+import com.sakuna63.sandbox.databinding.ActivityMainBinding;
+
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
