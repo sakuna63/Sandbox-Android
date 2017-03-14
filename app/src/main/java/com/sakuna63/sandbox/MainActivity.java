5aa3ade2b49a78a292b96afeb80eaad1d0b3a23e
Ippei Nawate <saku.ni63@gmail.com>
2017-03-14 19:33:17 +0900

    Experiment
    プロセスが殺されて復活する時、Activityの起動方法によってどのActivityが起動するか異なる
    
    startActivity：SecondActivityから起動する
    startActivityForResult：MainActivityから起動する

--- app/src/main/java/com/sakuna63/sandbox/MainActivity.java
+++ app/src/main/java/com/sakuna63/sandbox/MainActivity.java
@@ -1,14 +1,17 @@
 package com.sakuna63.sandbox;
 
-import com.sakuna63.sandbox.databinding.ActivityMainBinding;
-
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.sakuna63.sandbox.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult() called with: requestCode = [" + requestCode + "], resultCode = [" + resultCode + "], data = [" + data + "]");
     }
 }
--- app/src/main/java/com/sakuna63/sandbox/SecondActivity.java
+++ app/src/main/java/com/sakuna63/sandbox/SecondActivity.java
@@ -1,7 +1,7 @@
 package com.sakuna63.sandbox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +9,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("mykey", 1);
     }
 }
