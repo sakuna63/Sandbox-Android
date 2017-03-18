f3000ab99782e9835d49c9196dd1c10255ef8a2e
Ippei Nawate <saku.ni63@gmail.com>
2017-03-18 22:45:48 +0900

    Import
    https://www.youtube.com/watch?v=CPxkoe2MraA

--- app/src/main/java/com/sakuna63/sandbox/MainActivity.java
+++ app/src/main/java/com/sakuna63/sandbox/MainActivity.java
@@ -1,16 +1,38 @@
 package com.sakuna63.sandbox;
 
-import com.sakuna63.sandbox.databinding.ActivityMainBinding;
-
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.sakuna63.sandbox.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.image1.setOnClickListener(this);
+        binding.image2.setOnClickListener(this);
+        binding.image3.setOnClickListener(this);
+        binding.image4.setOnClickListener(this);
+        binding.image5.setOnClickListener(this);
+        binding.image6.setOnClickListener(this);
+        binding.image7.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int[] xy = new int[2];
+        v.getLocationOnScreen(xy);
+        Log.d("MainActivity", "xy:" + Arrays.toString(xy));
+        Log.d("MainActivity", "v.getLeft():" + v.getLeft());
+        Log.d("MainActivity", "v.getTop():" + v.getTop());
+        Log.d("MainActivity", "v.getX():" + v.getX());
+        Log.d("MainActivity", "v.getY():" + v.getY());
     }
 }
