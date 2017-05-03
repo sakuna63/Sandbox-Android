2cb9fbda6a3ec6bd5af2557b4cf5d05d74ade475
Ippei Nawate <saku.ni63@gmail.com>
2017-05-03 18:01:53 +0900

    イケそう

--- app/src/main/java/com/sakuna63/sandbox/MainActivity.java
+++ app/src/main/java/com/sakuna63/sandbox/MainActivity.java
@@ -1,10 +1,15 @@
 package com.sakuna63.sandbox;
 
-import com.sakuna63.sandbox.databinding.ActivityMainBinding;
-
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.sakuna63.sandbox.databinding.ActivityMainBinding;
+
+import org.parceler.Parcel;
+import org.parceler.ParcelConverter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,4 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
+
+    @Parcel(converter = DummyConverter.class)
+    interface Interface {
+
+    }
+
+    @Parcel
+    static class Data {
+        public List<? extends Interface> items;
+    }
+
+    static class DummyConverter implements ParcelConverter<Interface> {
+
+        @Override
+        public void toParcel(Interface input, android.os.Parcel parcel) {
+
+        }
+
+        @Override
+        public Interface fromParcel(android.os.Parcel parcel) {
+            return null;
+        }
+    }
+
 }
