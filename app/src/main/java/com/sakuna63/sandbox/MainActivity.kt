c61a8bfdd05ae5a27a9c75523b834fefebd30fbb
Ippei Nawate <saku.ni63@gmail.com>
2017-06-13 23:05:27 +0900

    落ちないけど表示されない（なぜ）

--- app/src/main/java/com/sakuna63/sandbox/MainActivity.kt
+++ app/src/main/java/com/sakuna63/sandbox/MainActivity.kt
@@ -2,13 +2,32 @@ package com.sakuna63.sandbox
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Looper
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.sakuna63.sandbox.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.root.setOnClickListener {
+            Log.d("TAG", "click")
+            thread {
+                Log.d("TAG", "in new thread")
+                if (Looper.myLooper() == null) {
+                    Looper.prepare()
+                }
+                AlertDialog.Builder(this@MainActivity)
+                        .setTitle("test")
+                        .setMessage("test test test")
+                        .show()
+                Log.d("TAG", "finish")
+            }
+        }
     }
 }
