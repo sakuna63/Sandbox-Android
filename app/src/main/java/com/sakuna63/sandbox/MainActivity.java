a8f84bd1ca7f3e945a99338fc5d8811f37edba24
Ippei Nawate <saku.ni63@gmail.com>
2017-06-17 12:09:43 +0900

    Disable data binding to speed up building

--- app/src/main/java/com/sakuna63/sandbox/MainActivity.java
+++ app/src/main/java/com/sakuna63/sandbox/MainActivity.java
@@ -1,8 +1,5 @@
 package com.sakuna63.sandbox;
 
-import com.sakuna63.sandbox.databinding.ActivityMainBinding;
-
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
